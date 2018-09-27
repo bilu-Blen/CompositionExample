8b53d176020aee72e90884ff23102f0e44b6e83f
bilu-Blen <azblen1@gmail.com>
2018-09-27 13:35:39 -0400

    Updated examples from class

--- src/me/afua/Main.java
+++ src/me/afua/Main.java
@@ -3,25 +3,37 @@ package me.afua;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
 	// Request the name of the parent
+        System.out.println("Please enter the name of the parent");
+        Scanner scan = new Scanner(System.in);
+        String parentName= scan.nextLine();
+
         Person theParent = new Person();
-        theParent.setName("Afua");
+        theParent.setName(parentName);
 
 //      Adding children here (as many children as the user would like)
         Person childOne = new Person();
-        childOne.setName("First Child");
+
+        System.out.println("Please enter the name of the first child");
+        String firstBorn = scan.nextLine();
+        childOne.setName(firstBorn);
         theParent.addAChild(childOne);
 
         Person childTwo = new Person();
-        childTwo.setName("Second Child");
+        System.out.println("Please enter the name of the second born");
+        String secondBorn = scan.nextLine();
+        childTwo.setName(secondBorn);
         theParent.addAChild(childTwo);
 
         Person childThree = new Person();
-        childThree.setName("Third Child");
+        System.out.println("Please enter the name of the third born");
+        String thirdBorn = scan.nextLine();
+        childThree.setName(thirdBorn);
         theParent.addAChild(childThree);
 
         ArrayList <String> theirNames  = new ArrayList<>();
@@ -33,7 +45,7 @@ public class Main {
         }
 
         Collections.sort(theirNames);
-        System.out.println("Sorted Names:\n");
+        System.out.println("Sorted Names of the children:\n");
         for(String aName:theirNames)
         {
             System.out.println(aName);
